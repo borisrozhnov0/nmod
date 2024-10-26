e06d16561ec36067547d65f53173988f37e9ddf4
borisrozhnov0 <borisrozhnov0@gmail.com>
2024-10-26 11:29:31 +0300

    feat(poly): now use mod n arithmetic.

--- test.cc
+++ test.cc
@@ -7,12 +7,13 @@ using namespace std;
 int main()
 {
     nmod::Poly<5> p;
-    p.set(1,1);
-    p.set(3,2);
+    p.set(1,53);
+    p.set(3,12);
     p.print(); cout << endl;
+    auto c = nmod::Poly<5>::mul(p, p);
     auto b = nmod::Poly<5>::shiftLeft(p, 1);
     b.print(); cout << endl;
-
-    cout <<"\n" << b(3) << endl;
+    c.print(); cout << endl;
+    cout <<"\n" << b(323) << endl;
     return 0;
 }
